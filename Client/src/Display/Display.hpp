b7d09d2e1fbfc1fa4766dd7a5e065cf70a3fb661
Yuuko <tilio.huart@epitech.eu>
2025-04-09 15:35:14 +0200

    feat (client): add basic class

---
+++ Client/src/Display/Display.hpp
@@ -0,0 +1,17 @@
+//
+// EPITECH PROJECT, 2025
+// Jetpack
+// File description:
+// Display.hpp
+//
+
+#pragma once
+
+namespace client {
+    class Display {
+       public:
+        Display();
+        ~Display();
+       private:
+    };
+}  // namespace client
---
+++ Client/src/Network/Network.hpp
@@ -0,0 +1,17 @@
+//
+// EPITECH PROJECT, 2025
+// Jetpack
+// File description:
+// Network.cpp
+//
+
+#pragma once
+
+namespace client {
+    class Network {
+       public:
+        Network();
+        ~Network();
+       private:
+    };
+}  // namespace client
