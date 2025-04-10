5aabe90219fef26b700ec79167d183aba96f96e4
Valiance <lucas.ludovic@epitech.eu>
2025-04-10 20:56:45 +0200

    fix(server): destroy server when unable to bind socket

--- Server/include/server.h
+++ Server/include/server.h
@@ -8,21 +8,24 @@
 #ifndef SERVER_H_
     #define SERVER_H_
 
+    #include "player.h"
     #include <stddef.h>
     #include <unistd.h>
-    #include "player.h"
 
     #define NB_PLAYER_MAX 2
 
 typedef struct server_s {
+    int is_debug;
+    int is_running;
+    char *map_file;
+
     struct pollfd *socket;
     struct sockaddr_in *address;
     int port;
     socklen_t socklen;
+
     size_t nb_player;
     player_t players[NB_PLAYER_MAX];
-    char *map_file;
-    int is_debug;
 
     int (*run)(struct server_s *this);
     void (*destroy)(struct server_s **this);
