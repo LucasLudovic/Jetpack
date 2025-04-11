955e864a05e134a0bc4b06dec79b1fe05351cb9d
Yuuko <tilio.huart@epitech.eu>
2025-04-11 09:01:43 +0200

    feat (client): create client class to handle connection to server

---
+++ Client/src/Network/socket/Socket.cpp
@@ -0,0 +1,31 @@
+//
+// EPITECH PROJECT, 2025
+// Jetpack
+// File description:
+// Socket.cpp
+//
+
+#include "Socket.hpp"
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+
+client::Socket::Socket(const std::string &ip, const std::string &port)
+{
+    this->_socket = socket(AF_INET, SOCK_STREAM, 0);
+    this->_address->sin_family = AF_INET;
+    this->_address->sin_port = htons(atoi(port.c_str()));
+    this->_address->sin_addr.s_addr = inet_addr(ip);
+    this->_addrlen = sizeof(*this->_address);
+}
+
+client::Socket::~Socket() {}
+
+void client::Socket::createConnection()
+{
+    if (connect(this->_socket, this->_address, this->_addrlen) == -1) {
+        throw SocketError();
+    }
+}
---
+++ Client/src/Network/socket/Socket.hpp
@@ -0,0 +1,45 @@
+//
+// EPITECH PROJECT, 2025
+// Jetpack
+// File description:
+// Socket.hpp
+//
+
+#pragma once
+
+#include <exception>
+#include <memory>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+
+namespace client {
+    class Socket {
+       public:
+        class SocketError : public std::exception {
+           public:
+            SocketError() { this->_msg = "Socket Error"; }
+
+            const char *what() const noexcept override
+            {
+                return this->_msg.c_str();
+            }
+
+           private:
+            std::string _msg;
+        };
+
+        Socket(const std::string &ip, const std::string &port);
+        ~Socket();
+
+        void createConnection();
+        void sendInput(client::Intput input) const;
+        const std::string &getServerInformation() const;
+
+       private:
+        int _socket;
+        int _port;
+        socklen_t _addrlen;
+        std::unique_ptr<struct sockaddr_in> _address;
+    };
+}  // namespace client
