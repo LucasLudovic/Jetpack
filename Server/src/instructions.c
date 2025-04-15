3145d89e5bac0ba3b946a51d6f512c486c3c5391
Valiance <lucas.ludovic@epitech.eu>
2025-04-15 14:47:44 +0200

    refacto(server): split move function in multiple subfunctions

--- Server/src/instructions.c
+++ Server/src/instructions.c
@@ -19,6 +19,58 @@
 const command_t commands[] = {
     {"PRESSED\r\n", move_up}, {"GETPOS\r\n", send_pos}, {NULL, NULL}};
 
+static void set_up_pos(
+    server_t *server, player_t *player, long time_since_last_ask)
+{
+    player->position.y += 0.1 * time_since_last_ask;
+    player->position.x += 0.1 * time_since_last_ask;
+    if (player->position.y > MAP_HEIGHT - 1) {
+        player->position.y = MAP_HEIGHT - 1;
+    }
+    if (player->position.x > strlen(server->map[0]) - 1) {
+        player->position.x = strlen(server->map[0]) - 1;
+        player->ended = TRUE;
+    }
+}
+
+static void set_down_pos(
+    server_t *server, player_t *player, long time_since_last_ask)
+{
+    player->position.y -= 0.05 * time_since_last_ask;
+    player->position.x += 0.1 * time_since_last_ask;
+    if (player->position.y < 0)
+        player->position.y = 0;
+    if (player->position.y >= MAP_HEIGHT - 1) {
+        player->position.y = MAP_HEIGHT - 1;
+    }
+    if (player->position.x > strlen(server->map[0]) - 1) {
+        player->position.x = strlen(server->map[0]) - 1;
+        player->ended = TRUE;
+    }
+}
+
+static void check_coin(
+    server_t *server, player_t *player, vector2_t *last_position)
+{
+    if (server->map[(size_t)player->position.y][(size_t)player->position.x] ==
+            'c' &&
+        (size_t)last_position->x != (size_t)player->position.x &&
+        (size_t)last_position->y != (size_t)player->position.y) {
+        player->score += 1;
+    }
+}
+
+static long compute_last_time(player_t *player, struct timespec *current_time)
+{
+    long time_since_last_ask = 0;
+
+    time_since_last_ask =
+        ((current_time->tv_sec - player->time_last_ask.tv_sec) * 1000) +
+        ((current_time->tv_nsec - player->time_last_ask.tv_nsec) / 1000000);
+    time_since_last_ask /= 100;
+    return time_since_last_ask;
+}
+
 void move_up(server_t *server, player_t *player)
 {
     char buff[BUFFSIZE];
@@ -27,36 +79,15 @@ void move_up(server_t *server, player_t *player)
     long time_since_last_ask = 0;
 
     clock_gettime(CLOCK_MONOTONIC, &current_time);
-
-    time_since_last_ask =
-        ((current_time.tv_sec - player->time_last_ask.tv_sec) * 1000) +
-        ((current_time.tv_nsec - player->time_last_ask.tv_nsec) / 1000000);
-    time_since_last_ask /= 100;
-
+    time_since_last_ask = compute_last_time(player, &current_time);
     if (player->ended == TRUE) {
         send(player->socket->fd, "GAME_END", strlen("GAME_END"), 0);
         return;
     }
-
     last_position.x = player->position.x;
     last_position.y = player->position.y;
-
-    player->position.y += 0.1 * time_since_last_ask;
-    player->position.x += 0.1 * time_since_last_ask;
-
-    if (player->position.y > MAP_HEIGHT - 1) {
-        player->position.y = MAP_HEIGHT - 1;
-    }
-    if (player->position.x > strlen(server->map[0]) - 1) {
-        player->position.x = strlen(server->map[0]) - 1;
-        player->ended = TRUE;
-    }
-    if (server->map[(size_t)player->position.y][(size_t)player->position.x] ==
-            'c' &&
-        (size_t)last_position.x != (size_t)player->position.x &&
-        (size_t)last_position.y != (size_t)player->position.y) {
-        player->score += 1;
-    }
+    set_up_pos(server, player, time_since_last_ask);
+    check_coin(server, player, &last_position);
     player->time_last_ask = current_time;
     snprintf(buff, BUFFSIZE, "position:x=%f:y%f\n", player->position.x,
         player->position.y);
@@ -71,38 +102,15 @@ void send_pos(server_t *server, player_t *player)
     long time_since_last_ask = 0;
 
     clock_gettime(CLOCK_MONOTONIC, &current_time);
-
-    time_since_last_ask =
-        ((current_time.tv_sec - player->time_last_ask.tv_sec) * 1000) +
-        ((current_time.tv_nsec - player->time_last_ask.tv_nsec) / 1000000);
-    time_since_last_ask /= 100;
-
+    time_since_last_ask = compute_last_time(player, &current_time);
     if (player->ended == TRUE) {
         send(player->socket->fd, "GAME_END", strlen("GAME_END"), 0);
         return;
     }
     last_position.x = player->position.x;
     last_position.y = player->position.y;
-
-    player->position.y -= 0.05 * time_since_last_ask;
-    player->position.x += 0.1 * time_since_last_ask;
-
-
-    if (player->position.y < 0)
-        player->position.y = 0;
-    if (player->position.y >= MAP_HEIGHT - 1) {
-        player->position.y = MAP_HEIGHT - 1;
-    }
-    if (player->position.x > strlen(server->map[0]) - 1) {
-        player->position.x = strlen(server->map[0]) - 1;
-        player->ended = TRUE;
-    }
-    if (server->map[(size_t)player->position.y][(size_t)player->position.x] ==
-            'c' &&
-        (size_t)last_position.x != (size_t)player->position.x &&
-        (size_t)last_position.y != (size_t)player->position.y) {
-        player->score += 1;
-    }
+    set_down_pos(server, player, time_since_last_ask);
+    check_coin(server, player, &last_position);
     player->time_last_ask = current_time;
     snprintf(buff, BUFFSIZE, "position:x=%f:y%f\n", player->position.x,
         player->position.y);
