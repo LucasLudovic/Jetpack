f6e89729dd787bb714acd4d52c74b49e9933195c
Yuuko <tilio.huart@epitech.eu>
2025-04-15 15:44:37 +0200

    chore: debug

--- Client/src/Network/Network.cpp
+++ Client/src/Network/Network.cpp
@@ -8,9 +8,11 @@
 #include "Network.hpp"
 #include "Inputs/Inputs.hpp"
 #include "Network/socket/Socket.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <sys/poll.h>
 
 client::Network::Network(const std::string &ip, const std::string &port)
 {
@@ -27,5 +29,13 @@ void client::Network::sendInputToServer(const std::string &msg) const
 
 std::string client::Network::retrieveServerInformation() const
 {
-    return this->_socket->getServerInformation();
+    struct pollfd *pfds = (struct pollfd *)malloc(sizeof(struct pollfd));
+    pfds[0].fd = this->_socket->getSocket();
+    pfds[0].events = POLLIN;
+    int rc = poll(pfds, 1, 33);
+    if (rc > 0) {
+        std::string msg = this->_socket->getServerInformation();
+    }
+    this->sendInputToServer(ok);
+    return msg;
 }
--- Client/src/Network/socket/Socket.hpp
+++ Client/src/Network/socket/Socket.hpp
@@ -36,6 +36,7 @@ namespace client {
         void createConnection();
         void sendInput(const std::string &msg) const;
         [[nodiscard]] std::string getServerInformation() const;
+        int getSocket();
 
        private:
         int _socket;
