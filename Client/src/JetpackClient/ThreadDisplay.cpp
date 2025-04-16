08e271b2dfdd2be34df0082d6890abd83d0fb2c3
Yuuko <tilio.huart@epitech.eu>
2025-04-16 11:21:48 +0200

    feat (client): retrieve map

--- Client/src/JetpackClient/ThreadDisplay.cpp
+++ Client/src/JetpackClient/ThreadDisplay.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "JetpackClient.hpp"
+#include <cstddef>
 #include <functional>
 #include <iostream>
 #include <map>
@@ -17,16 +18,53 @@ void client::JetpackClient::handleWaitingPlayers()
     this->_state = CLIENT_STATE::WAITING;
 }
 
+void client::JetpackClient::startMap()
+{
+    std::cout << "MAP START\n";
+    this->_retrieveMap = true;
+}
+
+void client::JetpackClient::retrieveMap(const std::string &map)
+{
+    size_t starting = 0;
+    size_t value = 0;
+
+    if (map.empty())
+        throw ClientError("Map was not send");
+
+    value = map.find('\n');
+
+    while ((value = map.find('\n', starting)) != std::string::npos) {
+        this->_map.push_back(map.substr(starting, value - starting));
+        starting = value + 1;
+    }
+    if (starting < map.size()) {
+        this->_map.push_back(map.substr(starting));
+    }
+}
+
+void client::JetpackClient::endMap()
+{
+    std::cout << "MAP END\n";
+    this->_retrieveMap = false;
+}
+
 void client::JetpackClient::handleDisplay()
 {
     std::map<std::string, std::function<void()>> instructions{
-        {"WAITING_PLAYERS\r\n",
+        {"WAITING_PLAYERS",
             [this] {
                 handleWaitingPlayers();
             }},
-        {"220 Connection established.\r\n", [this] {
-             handleWaitingPlayers();
-         }}};
+        {"START_MAP",
+            [this] {
+                startMap();
+            }},
+        {"END_MAP",
+            [this] {
+                endMap();
+            }},
+    };
 
     std::lock_guard<std::mutex> lock(this->data_mutex);
     if (this->_data.empty()) {
@@ -37,6 +75,11 @@ void client::JetpackClient::handleDisplay()
     for (const auto &it : instructions) {
         if (currentData == it.first) {
             it.second();
+            this->_data.pop();
+            break;
+        }
+        if (this->_retrieveMap) {
+            this->retrieveMap(currentData);
             this->_data.pop();
             break;
         }
--- Client/src/JetpackClient/ThreadNetwork.cpp
+++ Client/src/JetpackClient/ThreadNetwork.cpp
@@ -9,51 +9,12 @@
 #include <iostream>
 #include <string>
 
-void client::JetpackClient::retrieveMap()
-{
-    // auto map = this->_network.retrieveServerInformation();
-    // int starting = 0;
-    // int value = 0;
-    //
-    // if (map.empty())
-    //     throw ClientError("Map was not send");
-    //
-    // value = map.find('\n');
-    // while (value != std::string::npos) {
-    //     this->_map.push_back(map.substr(starting, value));
-    //     starting = value + 1;
-    // }
-    // for (const auto &it : this->_map) {
-    //     std::cout << "map = " << it << '\n';
-    // }
-}
-
-void client::JetpackClient::setupGame()
-{
-    // auto cmd = this->_network.retrieveServerInformation();
-    // std::cout << "cmd = " << cmd << '\n';
-    // if (cmd == "WAITING_PLAYERS\r\n") {
-    //     this->_state = CLIENT_STATE::WAITING;
-    // } else {
-    //     throw ClientError(
-    //         "Server must send Waiting players as first message");
-    // }
-    // std::string isMap;
-    // while (isMap.empty())
-    // std::cout << "Waiting Send Map = " << isMap << '\n';
-    // if (isMap == "SEND_MAP\r\n") {
-    //     this->retrieveMap();
-    // }
-}
-
 void client::JetpackClient::runNetworkThread()
 {
     while (this->_running) {
         this->_network.retrieveServerInformation();
         auto data = this->_network.getCommand();
 
-        if (!data.empty())
-            std::cout << "data = " << data << '\n';
         if (!data.empty()) {
             std::lock_guard<std::mutex> lock(this->data_mutex);
             this->_data.push(data);
