a6317a4943d7f32504581d2a459c68f1a762b41d
Valiance <lucas.ludovic@epitech.eu>
2025-04-17 17:24:00 +0200

    feat(client): render the assets

--- Client/src/Display/Display.cpp
+++ Client/src/Display/Display.cpp
@@ -177,4 +177,66 @@ void client::Display::_loadPlayerDieAssets()
 
 void client::Display::renderFrame(
     const Player &player, const std::vector<std::string> &map)
-{}
+{
+    const float spriteWidth = static_cast<float>(this->_window->getSize().x) /
+                              static_cast<float>(map[0].size());
+    const float spriteHeight = static_cast<float>(this->_window->getSize().y) /
+                               static_cast<float>(map.size());
+
+    static size_t frameCounter = 0;
+    const size_t coinFrame = frameCounter % _coin.size();
+    const size_t laserFrame = frameCounter % _laser.size();
+
+    this->_window->clear();
+    for (size_t y = 0; y < map.size(); ++y) {
+        for (size_t x = 0; x < map[y].size(); ++x) {
+            char tile = map[y][x];
+            sf::Vector2f position(x * spriteWidth, y * spriteHeight);
+
+            if (tile == 'c') {
+                auto &sprite = *this->_coin[coinFrame];
+                sprite.setPosition(position);
+                sprite.setScale(spriteWidth / sprite.getLocalBounds().width,
+                    spriteHeight / sprite.getLocalBounds().height);
+                this->_window->draw(sprite);
+            }
+            if (tile == 'e') {
+                auto &sprite = *this->_laser[laserFrame];
+                sprite.setPosition(position);
+                sprite.setScale(spriteWidth / sprite.getLocalBounds().width,
+                    spriteHeight / sprite.getLocalBounds().height);
+                this->_window->draw(sprite);
+            }
+        }
+    }
+    frameCounter += 1;
+    this->_drawPlayer(player, map);
+}
+
+void client::Display::_drawPlayer(
+    const Player &player, const std::vector<std::string> &map)
+{
+    static size_t frameCounter = 0;
+    std::unique_ptr<sf::Sprite> playerSprite;
+
+    const float tileWidth = static_cast<float>(this->_window->getSize().x) /
+                            static_cast<float>(map[0].size());
+    const float tileHeight = static_cast<float>(this->_window->getSize().y) /
+                             static_cast<float>(map.size());
+
+    const size_t runFrame = frameCounter % this->_playerRun.size();
+    const size_t flightFrame = frameCounter % this->_playerFlight.size();
+    const size_t dieFrame = frameCounter % this->_playerDie.size();
+
+    if (player.getPosY() > 0) {
+        playerSprite = std::move(this->_playerFlight[flightFrame]);
+    } else {
+        playerSprite = std::move(this->_playerRun[runFrame]);
+    }
+    playerSprite->setPosition(
+        player.getPosX() * tileWidth, player.getPosY() * tileHeight);
+    playerSprite->setScale(tileWidth / playerSprite->getLocalBounds().width,
+        tileHeight / playerSprite->getLocalBounds().height);
+    this->_window->draw(*playerSprite);
+    frameCounter += 1;
+}
